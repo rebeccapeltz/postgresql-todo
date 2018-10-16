e3e7187088558d68de6a69faa9bc65518e81c703
rebeccapeltz <rebeccapeltz@gmail>
2018-10-16 07:52:52 -0700

    add models and create routes

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
-// https://scotch.io/tutorials/getting-started-with-node-express-and-postgres-using-sequelize#toc-before-we-begin-
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
-// Set up the express app
 const app = express();
-
-// Log requests to the console.
 app.use(logger('dev'));
-
-// Parse incoming requests data (https://github.com/expressjs/body-parser)
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Setup a default catch-all route that sends back a welcome message in JSON format.
+require('./server/routes')(app);
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
 }));
--- server/models/index.js
+++ server/models/index.js
@@ -9,6 +9,8 @@ const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
+// export DATABASE_URL=our-database-url
+// see dotevv .env
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
